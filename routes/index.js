f06844aac3700bb1e27d0101d2dd4de92d8eae9e
ssnau <korige@gmail.com>
2013-03-11 20:43:16 +0800

    delete quirl acl

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,6 @@ var path = require('path')
 
 // Show the home page
 exports.index = function(req, res){
-    if (req.cookies.name != 'admin') {
-        res.send('you are not authorized!')
-        return;
-    }
     var file_tree = fm.path_to_json(app.get("config")['file_root'], {
       'file_filter': /\.md$|txt$/, //only md/txt allows
       'folder_ignore': /^_/ /* ignore those folder start with _*/
